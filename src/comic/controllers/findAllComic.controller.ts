50335f69c0defa9f6ca590408d9177abf0b8cfa7
DEISE C. SILVA <20192ewbj0133@belojardim.ifpe>
2023-06-29 21:43:37 -0300

    Paginação da rota de listagem  de quadrinhos

--- src/comic/controllers/findAllComic.controller.ts
+++ src/comic/controllers/findAllComic.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { findAllComicService } from "../services/findAllComic.service";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
@@ -7,10 +7,11 @@ import { ApiResponse, ApiTags } from "@nestjs/swagger";
 export class findAllComicController{
     constructor(private readonly comicService: findAllComicService) {}
 
-    @Get()
+    @Get('')
     @ApiResponse({ status: 201, description: 'The comics has been successfully findAll.'})
     @ApiResponse({ status: 403, description: 'Error'})
-    async findAll(pageNumber, itemNumber){
+    async findAll( @Query('pageNumber') pageNumber: number = 1,
+    @Query('itemNUmber') itemNumber: number = 10,){
         return this.comicService.findAll(pageNumber, itemNumber);
     }
 
--- src/comic/services/findAllComic.service.ts
+++ src/comic/services/findAllComic.service.ts
@@ -6,9 +6,11 @@ export class findAllComicService{
 
     constructor(private prisma:PrismaService){}
 
-    async findAll(pageNumber, itemNumber){
+    async findAll(pageNumber: number, itemNumber: number){
         const skip = (pageNumber - 1) * itemNumber;
         const take = itemNumber;
+        console.log(skip)
+        console.log(take)
         return this.prisma.comic.findMany({
             skip,
             take,
